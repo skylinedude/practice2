cb0d27b0844128a4bb235782009c6d0ed384cdb5
skylinedude <manoj1999gtr@gmail.com>
2024-01-16 22:49:50 +0530

    'l'

---
+++ fullstack/backend/language/java/all lesson/datatypes/src/DataType/Food.java
@@ -0,0 +1,21 @@
+package DataType;
+
+public class Food {
+	static String breakfast="pongal";
+	static int price1=40;
+	static String snacks="tea";
+	static int price2=10;
+	static String lunch="rice and curry";
+	static int price3=40;
+	static String dinner="chapathi, rice and curry";
+	static int price4=60;
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println(breakfast+" price "+price1);
+		System.out.println(snacks+" price "+price2);
+		System.out.println(lunch+" price "+price3);
+		System.out.println(dinner+" price "+price4);
+		
+	}
+
+}
